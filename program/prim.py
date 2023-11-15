71e0363086b35c54fd55f52a4f12a1c4dd1f0e0b
alexeiepif <aled2093746@gmail.com>
2023-11-15 20:44:39 +0300

    Code formatting

--- program/prim.py
+++ program/prim.py
@@ -1,33 +1,43 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+
 import sys
 from datetime import date
+
+
 if __name__ == '__main__':
     # Список работников.
     workers = []
+
     # Организовать бесконечный цикл запроса команд.
     while True:
         # Запросить команду из терминала.
         command = input(">>> ").lower()
+
         # Выполнить действие в соответствие с командой.
         if command == 'exit':
             break
+
         elif command == 'add':
             # Запросить данные о работнике.
             name = input("Фамилия и инициалы? ")
             post = input("Должность? ")
             year = int(input("Год поступления? "))
+
             # Создать словарь.
             worker = {
                 'name': name,
                 'post': post,
                 'year': year,
             }
+
             # Добавить словарь в список.
             workers.append(worker)
             # Отсортировать список в случае необходимости.
             if len(workers) > 1:
                 workers.sort(key=lambda item: item.get('name', ''))
+
         elif command == 'list':
             # Заголовок таблицы.
             line = '+-{}-+-{}-+-{}-+-{}-+'.format(
@@ -46,6 +56,7 @@ if __name__ == '__main__':
                 )
             )
             print(line)
+
             # Вывести данные о всех сотрудниках.
             for idx, worker in enumerate(workers, 1):
                 print(
@@ -56,15 +67,18 @@ if __name__ == '__main__':
                         worker.get('year', 0)
                     )
                 )
+
             print(line)
 
         elif command.startswith('select '):
             # Получить текущую дату.
             today = date.today()
+
             # Разбить команду на части для выделения номера года.
             parts = command.split(' ', maxsplit=1)
             # Получить требуемый стаж.
             period = int(parts[1])
+
             # Инициализировать счетчик.
             count = 0
             # Проверить сведения работников из списка.
@@ -74,9 +88,11 @@ if __name__ == '__main__':
                     print(
                         '{:>4}: {}'.format(count, worker.get('name', ''))
                     )
+
             # Если счетчик равен 0, то работники не найдены.
             if count == 0:
                 print("Работники с заданным стажем не найдены.")
+
         elif command == 'help':
             # Вывести справку о работе с программой.
             print("Список команд:\n")
@@ -85,5 +101,6 @@ if __name__ == '__main__':
             print("select <стаж> - запросить работников со стажем;")
             print("help - отобразить справку;")
             print("exit - завершить работу с программой.")
+
         else:
             print(f"Неизвестная команда {command}", file=sys.stderr)
